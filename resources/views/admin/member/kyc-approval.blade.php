1cf14b639bd3a3c7699f290b5ee0c729261781dc
Wayne Lai <waynelai06@gmail.com>
2023-03-27 14:36:18 +0800

    admin - fix dashboard ui and kyc approval copy id modal

--- resources/views/admin/member/kyc-approval.blade.php
+++ resources/views/admin/member/kyc-approval.blade.php
@@ -173,18 +173,28 @@
                                         </div>
                                         <!-- Modal body -->
                                         <div class="p-6 space-y-6">
-                                            <div class="grid grid-cols-2 gap-4">
-                                                <div class="flex justify-center item-center">
-                                                    <img
-                                                        src="{{ asset('uploads/users/' .$record->front_id_image)}}"
-                                                        alt="">
+                                            @if($record->front_id_image && $record->back_id_image)
+                                                <div class="grid grid-cols-2 gap-4">
+                                                    <div class="flex justify-center item-center">
+                                                        <img
+                                                            src="{{ asset('uploads/users/' .$record->front_id_image)}}"
+                                                            alt="">
+                                                    </div>
+                                                    <div class="flex justify-center item-center">
+                                                        <img
+                                                            src="{{ asset('uploads/users/' .$record->back_id_image)}}"
+                                                            alt="">
+                                                    </div>
                                                 </div>
-                                                <div class="flex justify-center item-center">
-                                                    <img
-                                                        src="{{ asset('uploads/users/' .$record->back_id_image)}}"
-                                                        alt="">
+                                            @else
+                                                <div class="w-full flex p-4 mb-4 text-sm text-warning-800 border border-warning-300 rounded-lg bg-warning-50 dark:bg-gray-800 dark:text-blue-400 dark:border-blue-800" role="alert">
+                                                    <svg aria-hidden="true" class="flex-shrink-0 inline w-5 h-5 mr-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
+                                                    <span class="sr-only">@lang('public.info')</span>
+                                                    <div>
+                                                        <span class="font-medium">@lang('public.info'):</span> @lang('public.no_record')
+                                                    </div>
                                                 </div>
-                                            </div>
+                                            @endif
                                         </div>
                                     </div>
                                 </div>
