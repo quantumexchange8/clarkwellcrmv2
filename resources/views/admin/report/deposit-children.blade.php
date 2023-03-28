9fe688a4b264ae0c60de6f786f6f99657a6ca427
Wayne Lai <waynelai06@gmail.com>
2023-03-28 15:52:49 +0800

    admin - fix import button ring focus color

--- resources/views/admin/report/deposit-children.blade.php
+++ resources/views/admin/report/deposit-children.blade.php
@@ -240,7 +240,7 @@
 
                         </div>
                         <div class="flex items-center  mb-4 justify-center w-full">
-                            <button type="submit" class="text-white bg-orange-400 hover:bg-orange-600 border border-orange-200 focus:ring-4 focus:outline-none focus:ring-blue-600 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="submit" value="import">@lang('public.import')</button>
+                            <button type="submit" class="text-white bg-orange-400 hover:bg-orange-600 border border-orange-200 focus:ring-4 focus:outline-none focus:ring-orange-400 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" name="submit" value="import">@lang('public.import')</button>
                         </div>
 
                     </form>
