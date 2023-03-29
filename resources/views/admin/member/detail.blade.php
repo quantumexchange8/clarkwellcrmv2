bad2eab0bf33dfe9d26ec47c00b94de4034a3feb
Wayne Lai <waynelai06@gmail.com>
2023-03-29 11:19:02 +0800

    admin - fix status missing

--- resources/views/admin/member/detail.blade.php
+++ resources/views/admin/member/detail.blade.php
@@ -118,20 +118,22 @@
                     <div class="mt-10 px-4">
                         <form action="{{ route('member_kyc_approval') }}" method="post">
                             @csrf
-                            <h2 class="font-semibold text-lg text-orange-400 mb-4">@lang('public.kyc_approval_status')</h2>
+                            <h2 class="font-semibold text-lg text-orange-400 mb-2">@lang('public.kyc_approval_status')</h2>
                             <span class="font-semibold text-lg text-gray-500">
                                 @if($user->kyc_approval_status == 3)
-                                    <span class="text-success font-semibold uppercase">@lang('public.kyc_verified')</span>
+                                    <span class="text-success font-semibold mr-2 mb-2 uppercase">@lang('public.kyc_verified')</span>
                                 @elseif($user->kyc_approval_status == 2)
-                                    <span class="text-warning font-semibold uppercase">@lang('public.kyc_pending_verify')</span>
+                                    <span class="text-warning font-semibold mr-2 mb-2 uppercase">@lang('public.kyc_pending_verify')</span>
+                                @elseif($user->kyc_approval_status == 4)
+                                    <span class="text-danger font-semibold mr-2 mb-2 uppercase">@lang('public.reject')</span>
                                 @elseif($user->kyc_approval_status == 1)
-                                    <span class="text-danger font-semibold uppercase">@lang('public.kyc_not_verify')</span>
+                                    <span class="text-danger font-semibold mr-2 mb-2 uppercase">@lang('public.kyc_not_verify')</span>
                                 @endif
                             </span>
                             @if($user->kyc_approval_status != App\Models\User::KYC_STATUS_VERIFIED)
                                 <input type="hidden" name="user_id" value="{{ $user->id }}">
                                 <input type="hidden" name="direct_approve" value="direct_approve">
-                                <button type="submit" name="approval" value="{{App\Models\User::KYC_STATUS_VERIFIED}}" class="text-white bg-success hover:bg-success-500 focus:ring-4 focus:outline-none focus:ring-success-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mx-2 dark:bg-success-600 dark:hover:bg-success-700 dark:focus:ring-success-800">
+                                <button type="submit" name="approval" value="{{App\Models\User::KYC_STATUS_VERIFIED}}" class="text-white bg-success hover:bg-success-500 focus:ring-4 focus:outline-none focus:ring-success-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-success-600 dark:hover:bg-success-700 dark:focus:ring-success-800">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mr-2 -ml-1">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" />
                                     </svg>
