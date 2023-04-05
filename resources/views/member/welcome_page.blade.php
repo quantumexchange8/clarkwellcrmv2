6987a7ffd21664b5555b7f281effd47e91bdc1bc
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-04-05 16:52:02 +0800

    fix: event pop up modal

--- resources/views/member/welcome_page.blade.php
+++ resources/views/member/welcome_page.blade.php
@@ -81,40 +81,63 @@
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </a>
         </div>
+        <div id="modal" class="fixed hidden z-50 inset-0 bg-gray-900 bg-opacity-60 overflow-y-auto h-full w-full px-4 modal">
+            <div class="relative top-5 mx-auto shadow-xl rounded-md bg-white max-w-xl">
+                @foreach($events as $event)
 
-        <!-- event modal -->
-        <div id="eventModal" tabindex="-1" class="fixed top-0 left-0 right-0 z-50 backdrop-blur hidden p-4 mx-auto overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-            <div class="relative w-full h-full mx-auto max-w-2xl md:h-auto">
-                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" id="closeEvent">
-                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    @foreach($events as $event)
-                        @if($event->pop_up_status)
-                            <div class="p-4 odd:bg-gray-100 even:bg-white rounded-l border border-gray-300">
-                                <h3 class="text-xl font-semibold text-[#FFA168] dark:text-white underline">
-                                    {{ $event->event_title }}
-                                </h3>
-                                <span class="text-xs font-semibold text-gray-500">{{ $event->created_at }}</span>
-                                <img class="mt-4 h-full my-auto" src="{{ asset('uploads/events/'.$event->event_image) }}" alt="">
-                            </div>
-                        @endif
-                    @endforeach
+                <!-- Modal header -->
+                <div class="flex justify-between items-center text-white text-xl rounded-t-md px-4 py-2">
+                    <h3 class="text-xl font-semibold text-[#FFA168] dark:text-white underline">
+                        {{ $event->event_title }}
+                    </h3>
+                    <button class="text-gray-500" onclick="closeModal('modal')">x</button>
+                </div>
+
+                <!-- Modal body -->
+                    @if($event->pop_up_status)
+                        <div class="p-4 odd:bg-gray-100 even:bg-white rounded-l border border-gray-300">
+                            <span class="text-xs font-semibold text-gray-500">{{ $event->created_at }}</span>
+                            <img class="mt-4 h-full my-auto object-contain" src="{{ asset('uploads/events/'.$event->event_image) }}" alt="">
+                        </div>
+                    @endif
+                @endforeach
+
+                <!-- Modal footer -->
+                <div class="px-4 py-2 border-t border-t-gray-200 flex justify-end items-center space-x-4">
+                    <button class="inline-block rounded bg-orange-100 px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-orange-700 transition duration-150 ease-in-out hover:bg-primary-accent-100 focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary-accent-200" onclick="closeModal('modal')">@lang('public.close')</button>
                 </div>
             </div>
         </div>
+
 @endsection
 
 @section('script')
     <script>
         $(document).ready(function(){
-            $('#eventModal').fadeIn('15');
-
-            $('#closeEvent').click(function (){
-                $('#eventModal').hide();
-            })
+            $('#modal').fadeIn('15');
         });
+
+        window.openModal = function(modalId) {
+            document.getElementById(modalId).style.display = 'block'
+            document.getElementsByTagName('body')[0].classList.add('overflow-y-hidden')
+        }
+
+        window.closeModal = function(modalId) {
+            document.getElementById(modalId).style.display = 'none'
+            document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+        }
+
+        // Close all modals when press ESC
+        document.onkeydown = function(event) {
+            event = event || window.event;
+            if (event.keyCode === 27) {
+                document.getElementsByTagName('body')[0].classList.remove('overflow-y-hidden')
+                let modals = document.getElementsByClassName('modal');
+                Array.prototype.slice.call(modals).forEach(i => {
+                    i.style.display = 'none'
+                })
+            }
+        };
 
     </script>
 @endsection
