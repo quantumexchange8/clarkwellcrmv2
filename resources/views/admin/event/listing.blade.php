64c41f860db37fe75ae526f16a6949bacce2bbec
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2023-04-06 10:00:30 +0800

    fix: create event button on listing page

--- resources/views/admin/event/listing.blade.php
+++ resources/views/admin/event/listing.blade.php
@@ -25,7 +25,7 @@
                             <svg class="h-6 w-6 text-white"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"/>
                             </svg>
-                            <span class="ml-4 text-md font-bold">@lang('public.create_news')</span>
+                            <span class="ml-4 text-md font-bold">@lang('public.create_event')</span>
                         </div>
                     </a>
                 </div>
