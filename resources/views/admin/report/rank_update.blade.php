59b6227b440dfe407d9251c554230c488268c089
zhengyong <kzy014@hotmail.com>
2023-04-19 09:58:13 +0800

    - fix the layout on ranking log table

--- resources/views/admin/report/rank_update.blade.php
+++ resources/views/admin/report/rank_update.blade.php
@@ -4,13 +4,13 @@
 
 @section('contents')
 
-    <h1 class="font-semibold text-2xl text-gray-500">@lang('public.reports') / @lang('public.monthly_lot')</h1>
+    <h1 class="font-semibold text-2xl text-gray-500">@lang('public.reports') / @lang('public.ranking_log')</h1>
 
     <!-- component -->
     <div class="flex flex-col">
         <form method="post" action="{{ route('ranking_logs_listing') }}">
             @csrf
-            <div class="grid gap-6 mb-6 mt-4 grid-cols-2 max-[1600px]:grid-cols-2 max-[1100px]:grid-cols-1">
+                <div class="grid gap-6 mb-6 mt-4 grid-cols-3 max-[1600px]:grid-cols-3 max-[1100px]:grid-cols-1">
                 <div class="relative">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                         <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
